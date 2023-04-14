3e9f593c266d1a27d5c73a26fc5e5694c42394aa
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-14 17:29:31 +0200

    seeded shoes table

--- database/seeders/ShoeSeeder.php
+++ database/seeders/ShoeSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Faker\Generator as Faker;
+use App\Models\Shoe;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,22 @@ class ShoeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 30; $i++) { 
+            $shoe = new Shoe;
+    
+            $shoe->marca = $faker->word();
+            $shoe->modello = $faker->word();
+            $shoe->colore = $faker->safeColorName();
+            $shoe->taglia = $faker->numberBetween(20, 46);
+            $shoe->prezzo = $faker->randomFloat(2, 20, 1000);
+            $shoe->costo = $faker->randomFloat(2, 20, 1000);
+            $shoe->genere = $faker->randomElement(['uomo', 'donna', 'bambino', 'bambina']);
+            $shoe->image = $faker->imageUrl(640, 480, 'shoes', true);
+
+            $shoe->save();
+        }
+
     }
 }
