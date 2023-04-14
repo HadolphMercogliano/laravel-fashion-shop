8fc09313d6795c21e18632c5b385ac24c57421b5
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-14 16:37:59 +0200

    migration

--- database/migrations/2023_04_14_140350_create_shoes_table.php
+++ database/migrations/2023_04_14_140350_create_shoes_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('shoes', function (Blueprint $table) {
             $table->id();
+            $table->string("marca", 50);
+            $table->string("modello", 50);
+            $table->string("colore", 50);
+            $table->tinyInteger("taglia");
+            $table->float("prezzo", 6, 2);
+            $table->float("costo", 6, 2);
+            $table->enum("genere", ['uomo', 'donna', 'bambino', 'bambina']);
             $table->timestamps();
         });
     }
