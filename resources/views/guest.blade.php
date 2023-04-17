fc288ec2c6caef4ce70e3d9aceb40935eb2a9f74
GioGio4 <g.ceccobelli@outlook.it>
2023-04-17 16:18:49 +0200

    Fix height card

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -2,10 +2,10 @@
 @section('content')
     <div class="container">
 
-        <div class="row justify-content-center align-items-center g-4">
+        <div class="row justify-content-center g-4 mb-3 ">
             @forelse ($shoes as $shoe)
                 <div class="col-4">
-                    <div class="card shadow">
+                    <div class="card shadow h-100 ">
                         <img src="{{ $shoe->image }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h4 class="card-title">Modello: {{ $shoe->marca }} - {{ $shoe->modello }}</h4>
@@ -20,6 +20,6 @@
                 <h4>Nessun prodotto...</h4>
             @endforelse
         </div>
+        {{ $shoes->links('pagination::bootstrap-5') }}
     </div>
-    {{ $shoes->links('pagination::bootstrap-5') }}
 @endsection
